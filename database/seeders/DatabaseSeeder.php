0826152f12be979dcab4d96f96e364f4d2ae9b70
trunk-kien <trungkienn10a6@gmail.com>
2026-08-07 00:05:27 +0700

    Them du lieu mau (Seeder) cho users, labs va equipment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Lab;
+use App\Models\Equipment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +19,34 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::create([
+            'name' => 'Cán bộ Quản lý',
+            'email' => 'admin@thuyloi.edu.vn',
+            'password' => Hash::make('12345678'), // Mật khẩu là 12345678
+            'role' => 'manager',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $lab1 = Lab::create([
+            'name' => 'Phòng Máy C1',
+            'location' => 'Tầng 3 Tòa C',
+        ]);
+
+        $lab2 = Lab::create([
+            'name' => 'Phòng Thực hành Mạng',
+            'location' => 'Tầng 4 Tòa C',
+        ]);
+
+        Equipment::create([
+            'lab_id' => $lab1->id,
+            'name' => 'Máy tính Dell Optiplex',
+            'total_qty' => 40,
+            'available_qty' => 40,
+        ]);
+        Equipment::create([
+            'lab_id' => $lab1->id,
+            'name' => 'Switch Cisco 2960',
+            'total_qty' => 2,
+            'available_qty' => 2,
         ]);
     }
 }
